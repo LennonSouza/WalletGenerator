45d582c277c4bce4e40fd4d7f95d975e11a1112b
lennon souza <lennon.souza@hotmail.com>
2022-04-04 10:40:06 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorWallet/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WalletGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Code Amount: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            List<string> codes = new List<string>();
+            for (int x = 0; x < amount; x++)
+            {
+                Random random = new Random();
+                string letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                char[] part1 = new char[5],
+                    part2 = new char[5],
+                    part3 = new char[5],
+                    part4 = new char[5];
+
+                int i = 0, z = 0, y = 0;
+                for (int j = 0; j < part1.Length; j++, i++, z++, y++)
+                {
+                    part1[j] = letras[random.Next(letras.Length)];
+                    part2[j] = letras[random.Next(letras.Length)];
+                    part3[j] = letras[random.Next(letras.Length)];
+                    part4[j] = letras[random.Next(letras.Length)];
+                }
+
+                string resultString = new String(part1),
+                    resultString2 = new String(part2),
+                    resultString3 = new String(part3),
+                    resultString4 = new String(part4),
+                    resultString5 = new String($"{resultString}-{resultString2}-{resultString3}-{resultString4}");
+
+                codes.Add(resultString5);
+                Console.WriteLine(resultString5);
+            }
+            File.WriteAllLines(@"Codes.txt", codes);
+        }
+    }
+}
